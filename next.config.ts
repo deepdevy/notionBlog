6ef60411113af2aa318d4269628f53fb342d81d0
deepdevy <ug928@naver.com>
2025-05-10 02:04:57 +0900

    create ui

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // experimental: {
+  //   typedRoutes: true,
+  // },
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'picsum.photos',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
