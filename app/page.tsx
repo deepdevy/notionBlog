0d56088d328104126beb49ebe0d061ea3bc828d1
deepdevy <ug928@naver.com>
2025-05-13 23:41:07 +0900

    route handler 이용

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,24 @@
-import { PostCard } from '@/components/features/blog/PostCard';
 import TagSection from '@/app/_components/TagSection';
 import ProfileSection from '@/app/_components/ProfileSection';
 // import ContactSection from '@/app/_components/ContactSection';
-import Link from 'next/link';
-import { getPublishedPosts, getTags } from '@/lib/notion';
-import SortSelect from '@/app/_components/SortSelect';
+import { getTags } from '@/lib/notion';
+// import { getPublishedPosts, getTags } from '@/lib/notion';
+import HeaderSection from '@/app/_components/HeaderSection';
+import PostListClient from '@/components/features/blog/PostList.client';
 interface HomeProps {
   searchParams: Promise<{ tag?: string; sort?: string }>;
 }
 
 export default async function Home({ searchParams }: HomeProps) {
-  const { tag, sort } = await searchParams;
+  const { tag } = await searchParams;
+  // const { tag, sort } = await searchParams;
   const selectedTag = tag || '전체';
-  const selectedSort = sort || 'latest';
-  const [posts, tags] = await Promise.all([
-    getPublishedPosts(selectedTag, selectedSort),
-    getTags(),
-  ]);
+  // const selectedSort = sort || 'latest';
+  const [tags] = await Promise.all([getTags()]);
+  // const [posts, tags] = await Promise.all([
+  //   getPublishedPosts(selectedTag, selectedSort),
+  //   getTags(),
+  // ]);
 
   return (
     <div className="container py-8">
@@ -27,21 +29,9 @@ export default async function Home({ searchParams }: HomeProps) {
         </aside>
         <div className="space-y-8">
           {/* 섹션 제목 */}
-          <div className="flex items-center justify-between">
-            <h2 className="text-3xl font-bold tracking-tight">
-              {selectedTag === '전체' ? '블로그 목록' : `${selectedTag} 관련 글`}
-            </h2>
-            <SortSelect />
-          </div>
-
+          <HeaderSection selectedTag={selectedTag} />
           {/* 블로그 카드 그리드 */}
-          <div className="grid gap-4">
-            {posts.map((post, index) => (
-              <Link href={`/blog/${post.slug}`} key={post.id}>
-                <PostCard post={post} isFirst={index === 0} />
-              </Link>
-            ))}
-          </div>
+          <PostListClient />
         </div>
         {/* 우측 사이드바 */}
         <aside className="flex flex-col gap-6">
